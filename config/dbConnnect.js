6ca6e7cf6ac9edfdc00ac9c2dddee5e8804183af
unknown <nihaltomail@gmail.com>
2017-10-06 23:52:18 +0530

    package json change

--- config/dbConnnect.js
+++ config/dbConnnect.js
@@ -1,10 +1,7 @@
 
 const config = require('./config.json');
 const MongoClient = require('mongodb').MongoClient;
-// connect with mongoClient// process.env.MONGOLAB_URI
-//MongoClient.connect(config['dbConnectionUrl'] + config['dbName'],  (err, db) => {
- // console.log(process.env.MONGOLAB_URI);
-MongoClient.connect("mongodb://test$#@@$@#:test$324@ds147884.mlab.com:47884/booklab",  (err, db) => {
+MongoClient.connect("mongodb://test:test@ds147884.mlab.com:47884/booklab",  (err, db) => {
 
       if (err) {
         console.log('Database connection error occur!', JSON.stringify(err));  
@@ -14,6 +11,28 @@ MongoClient.connect("mongodb://test$#@@$@#:test$324@ds147884.mlab.com:47884/book
         console.log('Database connection ok!');
         // assign db in to global object
         global['db'] = db;
+     
       }
     });
-  
+
+
+module.exports.connectDb = (cb) => {
+MongoClient.connect("mongodb://test:test@ds147884.mlab.com:47884/booklab",  (err, db) => {
+
+      if (err) {
+        console.log('Database connection error occur!', JSON.stringify(err));  
+        //throw new (Error(err));
+        cb(err,null);
+      }
+      else {
+        console.log('Database connection ok!');
+        // assign db in to global object
+        global['db'] = db;
+        cb(null,cb)
+      }
+    });
+
+}
+
+
+
