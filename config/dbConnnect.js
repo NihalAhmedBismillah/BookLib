7d6c208c052a5ad3ab4ed72f57bf819938451c9a
Nihal Ahmed <nihaltomail@gmail.com>
2017-10-15 00:20:12 +0530

    Update dbConnnect.js

--- config/dbConnnect.js
+++ config/dbConnnect.js
@@ -2,7 +2,7 @@
 const config = require('./config.json');
 const MongoClient = require('mongodb').MongoClient;
 //MongoClient.connect(config['dbConnectionUrl'] + config['dbName'], (err, db) => {
-MongoClient.connect("mongodb://test:test@ds147884.mlab.com:47884/booklab",  (err, db) => {
+MongoClient.connect("mongodb://test34@##:224!@test123@ds147884.mlab.com:47884/booklab",  (err, db) => {
 
   if (err) {
     console.log('Database connection error occur!', JSON.stringify(err));
